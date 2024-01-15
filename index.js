4151c594ae44477caa1a3cf8a6a6f62fcbd074dd
Sergey Krylov <ksv741@mail.ru>
2024-01-15 07:03:53 +0300

    create js, ts and react configurations

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 module.exports = {
-  extends: [
-    require.resolve('./rules/base'),
+  overrides: [
+    {
+      files: ['**/*.js'],
+      extends: [require.resolve('./js-base')],
+    },
+    {
+      files: ['**/*.jsx'],
+      extends: [require.resolve('./js-react')],
+    },
+    {
+      files: ['**/*.ts'],
+      extends: [require.resolve('./ts-base')],
+    },
+    {
+      files: ['**/*.tsx'],
+      extends: [require.resolve('./ts-react')],
+    },
   ],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {},
 };
---
+++ rules/react/.eslint-react-hooks.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    'react-hooks'
+  ],
+  rules: {
+    'react-hooks/exhaustive-deps': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+  }
+}
